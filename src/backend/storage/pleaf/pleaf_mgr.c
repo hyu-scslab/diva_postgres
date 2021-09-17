a28ae60646eb21ca180f5d66e32fa343a110b6b8
Jongbin Kim <jongbinkim@hanyang.ac.kr>
2021-09-17 17:33:06 +0900

    Modified license

--- src/backend/storage/pleaf/pleaf_mgr.c
+++ src/backend/storage/pleaf/pleaf_mgr.c
@@ -4,8 +4,20 @@
  * 		Provisional Index Manager Implementation 
  *
  * 
- * Portions Copyright (c) 1996-2020, PostgreSQL Global Development Group
- * Portions Copyright (c) 1994-5, Regents of the University of California
+ * Copyright (C) 2021 Scalable Computing Systems Lab.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  *
  * IDENTIFICATION
